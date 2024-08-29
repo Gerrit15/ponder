1e0069ec21f91a89d1287a3faacfdc01e313b242
Gerrit15 <overlorde159@gmail.com>
2024-08-29 13:39:21 -0400

    added query struct (incomplete)

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,50 @@ fn main() {
     connection.execute(query).unwrap();
 
     let mut values = vec![];
-    let query = "SELECT * FROM spells WHERE level = 0";
+    //let query = "SELECT * FROM spells WHERE level = 0 AND damage_type = 'Fire'";
+    let mut query = Query::new("spells".to_string(),"level".to_owned(), "=".to_owned(), QueryValue::Integer(0));
+    query.append("damage_type".to_string(), "=".to_owned(), QueryValue::Text("Fire".to_string()));
+    println!("{}", query.text);
 
-    connection.iterate(query, |pairs| {
+    connection.iterate(query.text, |pairs| {
         let pair = pairs[0].1.unwrap_or("None");
         values.push(pair.to_owned());
         true
     }).unwrap();
 
     for i in values {println!("{i}");}
+}
+
+
+#[derive(Debug)]
+struct Query {
+    text: String
+}
+
+impl Query {
+    fn new(table: String, field: String, operator: String, value: QueryValue) -> Query {
+        Query {text: "SELECT * FROM ".to_owned() + &table + " WHERE " + &field + " " + &operator + " " + &value.to_string()}
+    }
+    
+    fn append(&mut self, field: String, operator: String, value: QueryValue) {
+        self.text += &(" AND ".to_string() + &field + " " + &operator + " " + &value.to_string());
+    }
+}
+
+//Note that bool must be converted into text or a integer
+#[derive(Debug)]
+enum QueryValue {
+    Text(String),
+    Integer(i32),
+    Boolean(bool)
+}
+
+impl QueryValue {
+    fn to_string(&self) -> String {
+        return match self {
+            Self::Text(t) => t.to_owned(),
+            Self::Integer(i) => i.to_string(),
+            Self::Boolean(b) => b.to_string(),
+        }
+    }
 }
