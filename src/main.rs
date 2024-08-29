cd106b1e74fa3afa009421edeac4b56dcbdd720a
Gerrit15 <overlorde159@gmail.com>
2024-08-28 20:50:08 -0400

    Added basic search in sql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use sqlite;
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open(":memory:").unwrap();
+    let query = "
+        CREATE TABLE spells (name TEXT, level INTEGER, damage_type TEXT);
+        INSERT INTO spells VALUES ('Eldritch Blast', 0, 'Force');
+        INSERT INTO spells VALUES ('Fire Bolt', 0, 'Fire');
+        INSERT INTO spells VALUES ('Fireball', 3, 'Fire');
+    ";
+    connection.execute(query).unwrap();
+    let query = "SELECT * FROM spells WHERE level < 1";
+    connection.iterate(query, |pairs| {
+        println!("{:?}", pairs);
+        true
+    }).unwrap()
 }
