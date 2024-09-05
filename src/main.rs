b5f27e6b46e132036b4c50a8d9ccdf67fa117aa1
Gerrit15 <overlorde159@gmail.com>
2024-09-05 12:03:33 -0400

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlite;
+use sqlite::{self, Connection};
 use serde_json::Error;
 use serde::Deserialize;
 use std::fs;
@@ -6,61 +6,67 @@ use std::path::PathBuf;
 use std::io;
 use std::collections::HashMap;
 fn main() {
-    let connection = sqlite::open(":memory:").unwrap();
-    let mut setup = "
-        CREATE TABLE spells (
-            name TEXT, 
-            source TEXT,
-            level INTEGER,
-            School TEXT,
-            ritual INTEGER,
-            casting_time INTEGER,
-            casting_units TEXT,
-            v INTEGER,
-            s INTEGER,
-            m INTEGER,
-            component_cost INTEGER,
-            range INTEGER,
-            radius INTEGER,
-            shape TEXT,
-            text TEXT,
-            higher_level TEXT,
-            lists TEXT,
-            proc_eff TEXT,
-            proc_save TEXT,
-            damage1 INTEGER,
-            damage2 INTEGER,
-            damage3 INTEGER,
-            damage_types TEXT,
-            tags TEXT
-        );
-    ".to_string();
-
-    let spells = load_spells("/home/gerrit/projects/ponder/spells");
-    let mut spell_enums = SpellEnums::new();
-
-    for i in spells {
-        setup += &("INSERT INTO spells VALUES (".to_string() + &i.1.values() + ");");
-        spell_enums.update(&i.1);
-    }
-    println!("Sources: ");
-    for i in &spell_enums.sources {print!("{i}, ")}
-    println!();
-
-    connection.execute(setup).unwrap();
-
+    let (db, spell_enums) = Database::new("/home/gerrit/projects/ponder/spells");
+    for i in spell_enums.damage_types {print!("{i}, ")}
     let mut query = Query::new("spells","source", "=", QueryValue::Text("Player''s Handbook".to_owned()));
     query.append("level", "<=", QueryValue::Integer(1));
     query.append("damage_types", "=", QueryValue::Text(" Force".to_owned()));
 
     let mut values = vec![];
-    connection.iterate(query.text, |pairs| {
+    db.connection.iterate(query.text, |pairs| {
         let pair = pairs[0].1.unwrap_or("None");
         values.push(pair.to_owned());
         true
     }).unwrap();
     println!("Spells: ");
     for i in values {println!("{i}");}
+}
+
+struct Database {
+    connection: Connection
+}
+
+impl Database {
+    fn new(dir: &str) -> (Database, SpellEnums) {
+        let connection = sqlite::open(":memory:").unwrap();
+        let mut setup = "
+            CREATE TABLE spells (
+                name TEXT, 
+                source TEXT,
+                level INTEGER,
+                School TEXT,
+                ritual INTEGER,
+                casting_time INTEGER,
+                casting_units TEXT,
+                v INTEGER,
+                s INTEGER,
+                m INTEGER,
+                component_cost INTEGER,
+                range INTEGER,
+                radius INTEGER,
+                shape TEXT,
+                text TEXT,
+                higher_level TEXT,
+                lists TEXT,
+                proc_eff TEXT,
+                proc_save TEXT,
+                damage1 INTEGER,
+                damage2 INTEGER,
+                damage3 INTEGER,
+                damage_types TEXT,
+                tags TEXT
+        );".to_string();
+        let spells = load_spells(dir);
+        let mut spell_enums = SpellEnums::new();
+
+        for i in spells {
+            setup += &("INSERT INTO spells VALUES (".to_string() + &i.1.values() + ");");
+            spell_enums.update(&i.1);
+        }
+        connection.execute(setup).unwrap();
+        let db = Database {connection};
+        return (db, spell_enums)
+    }
 }
 
 #[derive(Debug)]
