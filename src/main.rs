e075dc0895a7e8fdde704933ac77d856504bce18
Gerrit15 <overlorde159@gmail.com>
2024-09-06 11:34:22 -0400

    Started on ratatui

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,24 @@ use std::collections::HashMap;
 use database::{Database, Query, QueryValue};
 use spellenums::SpellEnums;
 use spell::Spell;
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal, Frame
+};
+
+/*
+* TODO
+*  - Filtering with strings, should happen after get Vec<> from hashmap with a search
+*  - Frontend
+*  - idk make it nice
+*/
 
 fn main() {
-    let (db, spell_enums) = Database::new("/home/gerrit/projects/ponder/spells");
+    /*let (db, spell_enums) = Database::new("/home/gerrit/projects/ponder/spells");
     for i in spell_enums.damage_types {print!("{i}, ")}
     let mut query = Query::new("spells","source", "=", QueryValue::Text("Player''s Handbook".to_owned()));
-    query.append("level", "<=", QueryValue::Integer(1));
-    query.append("damage_types", "=", QueryValue::Text(" Force".to_owned()));
 
     let mut values = vec![];
     db.connection.iterate(query.text, |pairs| {
@@ -26,5 +37,45 @@ fn main() {
         true
     }).unwrap();
     println!("Spells: ");
-    for i in values {println!("{i}");}
+    for i in values {println!("{i}");}*/
+
+    App::new("TEST".to_owned()).start().unwrap();
 }
+
+#[derive(Debug)]
+struct App {
+    text: String,
+    exit: bool,
+}
+
+impl App {
+    fn new(text: String) -> App {
+        App { text, exit: false }
+    }
+    fn start(&mut self) -> io::Result<()> {
+        let mut terminal = ratatui::init();
+        terminal.clear()?;
+        let app_result = self.run(&mut terminal);
+        ratatui::restore();
+        app_result
+    }
+
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        todo!()
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        todo!()
+    }
+    //Pickup at Displaying the application https://ratatui.rs/tutorials/counter-app/basic-app/
+}
+
+
