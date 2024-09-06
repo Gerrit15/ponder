03a0a7f161fd8a074d16b8553c052c212d81e59a
Gerrit15 <overlorde159@gmail.com>
2024-09-06 15:01:01 -0400

    Refactored & TUI

--- src/app.rs
+++ src/app.rs
@@ -1,70 +1,16 @@
-mod database;
-mod spellenums;
-mod spell;
-mod app;
-use sqlite::{self, Connection};
-use serde_json::Error;
-use serde::Deserialize;
-use std::fs;
-use std::path::PathBuf;
-use std::io;
-use std::collections::HashMap;
-use database::{Database, Query, QueryValue};
-use spellenums::SpellEnums;
-use spell::Spell;
-use app::App;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    buffer::Buffer,
-    layout::{Alignment, Rect},
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{
-        block::{Position, Title},
-        Block,
-        Paragraph,
-        Widget
-    },
-    DefaultTerminal, Frame
-};
-
-/*
-* TODO
-*  - Filtering with strings, should happen after get Vec<> from hashmap with a search
-*  - Frontend
-*  - idk make it nice
-*/
-
-fn main() {
-    /*let (db, spell_enums) = Database::new("/home/gerrit/projects/ponder/spells");
-    for i in spell_enums.damage_types {print!("{i}, ")}
-    let mut query = Query::new("spells","source", "=", QueryValue::Text("Player''s Handbook".to_owned()));
-
-    let mut values = vec![];
-    db.connection.iterate(query.text, |pairs| {
-        let pair = pairs[0].1.unwrap_or("None");
-        values.push(pair.to_owned());
-        true
-    }).unwrap();
-    println!("Spells: ");
-    for i in values {println!("{i}");}*/
-
-    App::new("TEST".to_owned()).start().unwrap();
-}
-
-/*#[derive(Debug)]
-struct App {
-    text: String,
-    number: u32,
-    exit: bool,
+use crate::*;
+#[derive(Debug)]
+pub struct App {
+    pub text: String,
+    pub number: u32,
+    pub exit: bool,
 }
 
 impl App {
-    fn new(text: String) -> App {
+    pub fn new(text: String) -> App {
         App { text, number: 0, exit: false }
     }
-    fn start(&mut self) -> io::Result<()> {
+    pub fn start(&mut self) -> io::Result<()> {
         let mut terminal = ratatui::init();
         terminal.clear()?;
         let app_result = self.run(&mut terminal);
@@ -72,7 +18,7 @@ impl App {
         app_result
     }
 
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
@@ -80,11 +26,11 @@ impl App {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
+    pub fn draw(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
     }
 
-    fn handle_events(&mut self) -> io::Result<()> {
+    pub fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
@@ -94,7 +40,7 @@ impl App {
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
+    pub fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
             KeyCode::Left => self.decrement_counter(),
@@ -103,13 +49,13 @@ impl App {
         }
     }
 
-    fn exit(&mut self) {
+    pub fn exit(&mut self) {
         self.exit = true;
     }
-    fn increment_counter(&mut self) {
+    pub fn increment_counter(&mut self) {
         self.number += 1;
     }
-    fn decrement_counter(&mut self) {
+    pub fn decrement_counter(&mut self) {
         self.number -= 1;
     }
 }
@@ -139,4 +85,4 @@ impl Widget for &App {
             .block(block)
             .render(area, buf)
     }
-}*/
+}
