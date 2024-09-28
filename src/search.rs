4d71c285f919dc4525f9ae1ee4a59a425c455457
Gerrit15 <overlorde159@gmail.com>
2024-09-27 22:21:29 -0400

    did the thing

--- src/search.rs
+++ src/search.rs
@@ -1,17 +1,21 @@
-use ratatui::{prelude::{Constraint, Layout}, layout::Flex, widgets::Clear};
+use ratatui::{prelude::{Constraint, Layout}, layout::Flex, widgets::{Clear, ListState, List}, style::{Style, Modifier}};
 
 use crate::*;
 
 pub struct Search {
     spell_enums: SpellEnums,
-    popup: bool
+    popup: bool,
+    tags_state: ListState,
+    t: String
 }
 
 impl Search {
     pub fn new(spell_enums: SpellEnums) -> Search {
         Search {
             spell_enums,
-            popup: false
+            popup: false,
+            tags_state: ListState::default(),
+            t: "".to_string()
         }
     }
 }
@@ -19,15 +23,26 @@ impl Search {
 impl Page for Search {
     fn draw_page(&mut self, frame: &mut Frame, layout: Rect) {
         let area = popup_area(layout, 60, 20);
-        frame.render_widget(Paragraph::new("AAAAAAAA"), layout);
+        frame.render_widget(Paragraph::new(self.t.clone()), layout);
         if self.popup {
+            let tags = List::new(self.spell_enums.tags.clone())
+                .block(Block::bordered().title("TAGS"))
+                .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
+                .highlight_symbol(">")
+                .repeat_highlight_symbol(true);
             frame.render_widget(Clear, area);
-            frame.render_widget(Paragraph::new(self.spell_enums.tags.join(" ")), area)
+            frame.render_stateful_widget(tags, area, &mut self.tags_state);
         }
     }
     fn key(&mut self, key: KeyCode) {
         match key {
             KeyCode::Char(' ') => self.popup = !self.popup,
+            KeyCode::Char('j') => if self.popup { self.tags_state.select_next() }
+            KeyCode::Char('k') => if self.popup { self.tags_state.select_previous() }
+            KeyCode::Enter => if self.popup {match self.tags_state.selected() {
+                Some(n) =>  self.t += &self.spell_enums.tags[n].clone(),
+                _ => ()
+            }}
             _ => ()
         }
     }
